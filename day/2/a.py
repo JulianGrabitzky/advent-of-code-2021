9cc4d1beb423161669fe49f3c8d24bae9e2f4b86
Julian Grabitzky <grabitzkyjulian@gmail.com>
2021-12-02 06:24:29 +0100

    Day 2

---
+++ day/2/a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+def get_input():
+    with open('input/2.txt') as f:
+        return f.read().splitlines()
+
+h = 0
+d = 0
+
+for move in get_input():
+    x = move.split()
+    match x[0]:
+        case 'forward':
+            h += int(x[1])
+        case 'up':
+            d -= int(x[1])
+        case 'down':
+            d += int(x[1])
+result = d * h
+
+print('Depth: {:d}, H: {:d}, Result: {:d}'.format(d,h,result))
